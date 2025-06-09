36636e45c125ff8c15fc64b2a96080b5f04e4f2c
Igor Kulkov <iakulkov@avito.ru>
2025-06-09 17:26:16 +0300

    cleanup

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -11,6 +11,7 @@
     ];
     plugins = with pkgs.vimPlugins; [
       { plugin = catppuccin-nvim; type = "lua"; config = builtins.readFile ./neovim/plugins/catppuccin-nvim.lua; }
+      { plugin = neo-tree-nvim; type = "lua"; config = builtins.readFile ./neovim/plugins/neo-tree-nvim.lua; }
       { plugin = nvim-colorizer-lua; type = "lua"; config = builtins.readFile ./neovim/plugins/nvim-colorizer-lua.lua; }
       { plugin = guess-indent-nvim; type = "lua"; config = builtins.readFile ./neovim/plugins/guess-indent-nvim.lua; }
       { plugin = gitsigns-nvim; type = "lua"; config = builtins.readFile ./neovim/plugins/gitsigns-nvim.lua; }
