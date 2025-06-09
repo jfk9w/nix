1024aab226eb1ec16493672bbfd7cf815b1804a0
Igor Kulkov <iakulkov@avito.ru>
2025-06-09 17:48:27 +0300

    cleanup

--- home-manager/neovim/plugins/conform-nvim.lua
+++ home-manager/neovim/plugins/conform-nvim.lua
@@ -1,11 +1,17 @@
-require("conform").setup({
-    formatters_by_ft = {
-        lua = { "stylua" },
-        nix = { "alejandra" },
-    },
-    format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_format = "fallback",
-    },
-});
+(function()
+    require("conform").setup({
+        formatters_by_ft = {
+            lua = { "stylua" },
+            nix = { "alejandra" },
+        },
+        format_on_save = {
+            -- These options will be passed to conform.format()
+            timeout_ms = 500,
+            lsp_format = "fallback",
+        },
+    })
+
+    vim.keymap.set('n', '<leader>f', function()
+        require('conform').format { async = true, lsp_format = 'fallback' }
+    end, { desc = '[F]ormat buffer' })
+end)()
