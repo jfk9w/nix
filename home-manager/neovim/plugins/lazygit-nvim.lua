0e850153e1d2d6e1fb6c9511b830ff213295a3ef
Igor Kulkov <iakulkov@avito.ru>
2025-06-12 01:21:35 +0300

    refresh neotree on lazygit exit

--- home-manager/neovim/plugins/lazygit-nvim.lua
+++ home-manager/neovim/plugins/lazygit-nvim.lua
@@ -2,4 +2,14 @@ do
   vim.keymap.set("n", "<leader>lg", function()
     require("lazygit").lazygit(vim.fn.getcwd())
   end, { desc = "[L]azy [G]it" })
+
+  vim.api.nvim_create_autocmd({ "BufLeave" }, {
+    pattern = { "*lazygit*" },
+    group = vim.api.nvim_create_augroup("git_refresh_neotree", { clear = true }),
+    callback = function()
+      require("neo-tree.sources.filesystem.commands").refresh(
+        require("neo-tree.sources.manager").get_state("filesystem")
+      )
+    end,
+  })
 end
